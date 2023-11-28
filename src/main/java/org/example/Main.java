ce4c317d23b474dd81ccc27cb0d904742ec06f77
Jacaalus <jacaalus@gmail.com>
2023-11-28 11:29:40 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Natillera la abuelita toretto
+        //¿Que necesito para procesar la natillera de 1 cliente?
+        //ENTRADAS
+        int identificadorCliente=1234;
+        long documentoIdentidad=1003079509000L;
+        byte edadCliente=22;
+        short cuotaMensualCliente=25000;
+        double tasaInteresFijo=0.1;
+        char calificacionCliente='a';
+        
+    }
+}
